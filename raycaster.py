b4e9ebacc310fc7a60b559bd305c2a4398c0ca4a
GabrielSDC <gabrielsantosdacosta2@gmail.com>
2024-05-06 16:37:32 -0300

    calculating horizontal and vertical rays, but only displaying the shortest

--- raycaster.py
+++ raycaster.py
@@ -14,11 +14,12 @@ pixel   = 10
 font    = pygame.font.get_default_font()
 wr_font = pygame.font.SysFont(font, 28, True, False)
 
-black = (  0,   0,   0)
-white = (255, 255, 255)
-grey  = (127, 127, 127)
-red   = (255,   0,   0)
-green = (  0,   0, 255)
+black  = (  0,   0,   0)
+white  = (255, 255, 255)
+grey   = (127, 127, 127)
+red    = (255,   0,   0)
+green  = (  0,   0, 255)
+yellow = (255, 255,   0)
 
 maps = [
     1, 1, 1, 1, 1, 1, 1,
@@ -73,21 +74,56 @@ def display_player():
     vision = (player.x + cos(player.z) * 20, player.y + sin(player.z) * 20)
     pygame.draw.line(screen, red, (player.x, player.y), vision, 2) 
 
-def calc_distance(x1: int, y1: int, x2: int, y2: int) -> float:
+def calc_distance(pos1: tuple, pos2: tuple) -> float:
+    x1, y1 = pos1
+    x2, y2 = pos2
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
 def display_arrays():
-    # vertical calculation
     ix, iy = 0, 0
+
+    # horizontal calculation
+    h_distance = 0
+
     if player.z > 180 and player.z < 360:
         iy = player.y % block
         ix = iy / tan(player.z)
-        pygame.draw.line(screen, green, (player.x, player.y), (player.x - ix, player.y - iy))
+        
+        h_iray = (player.x - ix, player.y - iy)
     elif player.z > 0 and player.z < 180:
         iy = block - player.y % block
         ix = iy * -tan(player.z + 90)
-        pygame.draw.line(screen, green, (player.x, player.y), (player.x + ix, player.y + iy))
+        
+        h_iray = (player.x + ix, player.y + iy)
+    else:
+        h_iray = (width if player.z == 0 else 0, player.y)
 
+    h_distance = calc_distance((player.x, player.y), h_iray)
+    
+    # vertical calculation
+    v_distance = 0
+
+    if player.z > 90 and player.z < 270:
+        ix = player.x % block
+        iy = ix * tan(player.z)
+
+        v_iray = (player.x - ix, player.y - iy)
+    elif player.z < 90 or player.z > 270:
+        ix = block - player.x % block
+        iy = ix * tan(player.z)
+
+        v_iray = (player.x + ix, player.y + iy)    
+    else:
+        v_iray = (0 if player.z == 90 else height, player.x)
+
+    v_distance = calc_distance((player.x, player.y), v_iray)
+    
+    if v_distance < h_distance:
+        pygame.draw.line(screen, yellow, (player.x, player.y), v_iray)
+    else:
+        pygame.draw.line(screen, yellow, (player.x, player.y), h_iray)
+
+    return v_distance, h_distance
 
 while running:
     for event in pygame.event.get():
@@ -99,10 +135,11 @@ while running:
     screen.fill(grey)
 
     display_map()
-    display_arrays()
+    v, h = display_arrays()
     display_player()
 
-    screen.blit(wr_font.render(f"{player.x:.1f}, {player.y:.1f}, {player.z:.1f}", True, black), (0, 0))
+    # screen.blit(wr_font.render(f"{player.x:.1f}, {player.y:.1f}, {player.z:.1f}", True, black), (0, 0))
+    screen.blit(wr_font.render(f"v: {v:.1f}, h: {h:.1f}", True, black), (0, 0))
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
